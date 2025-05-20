6a4c3dd1ee8717dbca2d3f7dc39dfe419362f017
gomez7373 <sgc.holberton@gmail.com>
2025-05-19 22:56:42 -0400

    tasks

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Controlador del menú principal. Gestiona el botón Play.
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Carga la escena del laberinto cuando se presiona Play.
+    /// </summary>
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene("maze");
+    }
+}
